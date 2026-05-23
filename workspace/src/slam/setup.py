439ec06092de68907759e36b1855a8877a98eca8
Abhishek Jain <cs1240328@cse.iitd.ac.in>
2026-05-23 07:11:51 +0000

    added teleop

---
+++ workspace/src/slam/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'slam'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='cs1240328@cse.iitd.ac.in',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'drone_teleop = slam.drone_teleop:main',
+        ],
+    },
+)
